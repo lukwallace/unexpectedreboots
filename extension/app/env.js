abca9580736add34fa0ede5c8607c905ab53c681
Stephen Cefali <stephencefali@gmail.com>
2016-11-01 15:10:05 -0700

    Fixes bug with go to dashboard button not working properly

--- extension/app/env.js
+++ extension/app/env.js
@@ -4,7 +4,9 @@
   // API url
   window.__env.apiUrl = 'http://162.243.154.104';
   window.__env.port = '3000';
+  window.__env.livePort = '8080';
   window.__env.destUrl = window.__env.apiUrl + ':' + window.__env.port;
+  window.__env.liveUrl = window.__env.apiUrl + ':' + window.__env.livePort;
 
   // Base url
   window.__env.baseUrl = '/';
--- extension/controllers/main.js
+++ extension/controllers/main.js
@@ -1,15 +1,15 @@
 angular.module('mainController', ['ui.router'])
 
 .controller('main', function($scope, $http, $location, $state, __env) {
-  const destUrl = __env.destUrl;
+  const liveUrl = __env.liveUrl;
   $scope.testSession = function() {
-    chrome.tabs.create({url: destUrl + '/dashboard.html'})
+    chrome.tabs.create({url: liveUrl + '/client/dashboard.html'})
   },
   $scope.signOut = function() {
-    chrome.cookies.remove({url: destUrl + '/dashboard.html', name: 'username'}, function(removedCookie) {
+    chrome.cookies.remove({url: liveUrl + '/client/dashboard.html', name: 'username'}, function(removedCookie) {
       console.log(removedCookie);
     });
-    chrome.cookies.remove({url: destUrl + '/', name: 'connect.sid'}, function(removedCookie) {
+    chrome.cookies.remove({url: liveUrl + '/', name: 'connect.sid'}, function(removedCookie) {
       console.log(removedCookie);
       localStorage.removeItem('username');
       $state.transitionTo('login');
