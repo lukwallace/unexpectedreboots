b5c178d3d67ad33c5715a8bc550700bfaeb9fc63
Stephen Cefali <stephencefali@gmail.com>
2016-11-01 14:44:49 -0700

    updates env

--- extension/controllers/dropdown.js
+++ extension/controllers/dropdown.js
@@ -6,13 +6,14 @@ angular.module('dropdownController', [])
   $scope.usernamePattern = '^$|^[a-z]+[A-Za-z0-9_-.]+';
   $scope.showError = false;
 
-  console.log('env', __env);
+  const destUrl = __env.destUrl;
+  localStorage.setItem('destUrl', destUrl);
 
   $scope.signUp = function() {
 
     $http({ 
       method: 'POST',
-      url: 'http://104.237.1.118:3000/api/users/register',
+      url: destUrl + '/api/users/register',
       params: {
         email: $scope.email, 
         username: $scope.username, 
@@ -23,7 +24,7 @@ angular.module('dropdownController', [])
       console.log('then statement sign up', response);
       if (response.data) {
         chrome.cookies.set({
-          url: 'http://104.237.1.118:3000/dashboard.html',
+          url: destUrl + '/dashboard.html',
           name: 'username',
           value: $scope.username
         }, function(cookies) {
@@ -49,7 +50,7 @@ angular.module('dropdownController', [])
 
     $http({ 
       method: 'POST',
-      url: 'http://104.237.1.118:3000/api/users/login',
+      url: destUrl + '/api/users/login',
       params: {
         username: $scope.username, 
         password: $scope.password
@@ -61,7 +62,7 @@ angular.module('dropdownController', [])
         $scope.showError = false;
         localStorage.setItem('username', $scope.username);
         chrome.cookies.set({
-          url: 'http://104.237.1.118:3000/dashboard.html',
+          url: destUrl + '/dashboard.html',
           name: 'username',
           value: $scope.username
         }, function(cookies) {
