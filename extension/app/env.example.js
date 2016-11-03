0645d2fb963d7703ecd2808cc25a38a049b13ddf
Wallace Luk <wallace@Wallaces-MacBook-Air.local>
2016-11-03 10:23:51 -0700

    Add config and env to gitignore

---
+++ extension/app/env.example.js
@@ -0,0 +1,18 @@
+//Uncomment the service below and add your server ip
+/* (function (window) {
+  window.__env = window.__env || {};
+
+  // API url
+  window.__env.apiUrl = 'IP_FOR_SERVER';
+  window.__env.port = '3000'; //Port for server
+  window.__env.livePort = '8080'; //Port for client-side (dashboard)
+  window.__env.destUrl = window.__env.apiUrl + ':' + window.__env.port;
+  window.__env.liveUrl = window.__env.apiUrl + ':' + window.__env.livePort;
+
+  // Base url
+  window.__env.baseUrl = '/';
+
+  // Whether or not to enable debug mode
+  // Setting this to false will disable console output
+  window.__env.enableDebug = true;
+}(this)); */
